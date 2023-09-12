3e38f077d3a82126e015ce0d1b0843e78b437032
Bere-MP <113536555+Bere-MP@users.noreply.github.com>
2023-09-11 22:51:15 -0500

    Update function.h

--- function.h
+++ function.h
@@ -6,7 +6,11 @@
 
 using namespace std;
 
+// This function finds the index using the number provided by the user
 int findIndex(vector<int> &dataSet, int num);
+// This function goes to the index provided and modifys the value
 int modifyValue(vector<int> &dataSet, int index, int newNum);
+// This function add a number to the vector
 void addInteger(vector<int> &dataSet, int num);
+// This function removes the integer using the index the user provided
 void Remove(vector<int> &dataSet, int index);
